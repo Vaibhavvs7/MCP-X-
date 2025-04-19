5271ee78ca1d2a29b9a415bc025ad26513a57199
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-04-19 20:43:56 +0530

    Implement chat functionality using GoogleGenAI and dotenv for environment variables

--- client/index.js
+++ client/index.js
@@ -1,1 +1,25 @@
-import readLine from 'readline/promises';
+require("dotenv").config();
+const readLine = require("readline/promises");
+const { GoogleGenAI } = require("@google/genai");
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+const chatHistory = [];
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+async function chatLoop() {
+  const question = await rl.question("You: ");
+
+  chatHistory.push({ role: "user", parts: [{ text: question, type: "text" }] });
+  const response = await ai.models.generateContent({
+    model: "gemini-1.5-flash",
+    contents: chatHistory,
+  });
+
+  console.log(response.candidates[0].content.parts[0].text);
+  chatLoop();
+}
+
+chatLoop();

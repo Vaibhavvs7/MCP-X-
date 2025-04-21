8d973987433f17c2e84dbce2ee9d48191727bfdc
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-04-21 15:08:16 +0530

    Refactor index.js to use ES module syntax and update package.json to specify module type

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
-const readLine = require("readline/promises");
-const { GoogleGenAI } = require("@google/genai");
-const { type } = require("os");
+import {config}  from "dotenv";
+import readLine from "readline/promises";
+import { GoogleGenAI } from "@google/genai";
+config();
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
